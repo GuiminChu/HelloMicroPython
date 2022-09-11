35a446d8c7281edb5c06672094a28ae61f61b4e7
Guimin Chu <455824173@qq.com>
2022-09-11 09:57:13 +0800

    Digital Humidity & Temperature

---
+++ test_2_dht22.py
@@ -0,0 +1,16 @@
+from machine import Pin
+import dht
+import json
+
+dht22 = dht.DHT22(Pin(15))
+dht22.measure()
+print(dht22.temperature())
+print(dht22.humidity())
+
+d1 = {
+    "temperature": dht22.temperature(),
+    "humidity": dht22.humidity()
+}
+
+j1 = json.dumps(d1)
+print(j1)
